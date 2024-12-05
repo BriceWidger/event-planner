e5d761e97949814a201cb9464c1490657e1d1e8d
Brice <59327804+BriceWidger@users.noreply.github.com>
2024-12-05 16:46:32 -0500

    Added more components. Continued to build the TicketsPage.jsx

---
+++ src/components/ArrowDivider.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ArrowDivider = () => {
+  return (
+    <span
+      className="arrow-divider"
+      style={{
+        fontSize: "100px",
+        color: "#ccc",
+        fontWeight: "100",
+        marginTop: "-30px",
+        lineHeight: "59.1333px",
+        transform: "scaleX(0.5)",
+        marginRight: "0px",
+      }}
+    >
+      ›
+    </span>
+  );
+};
+
+export default ArrowDivider;
--- src/components/TicketsPage.jsx
+++ src/components/TicketsPage.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import ArrowDivider from "./ArrowDivider";
 
 const TicketsPage = () => {
   return (
     <div
-      className="vw-100 d-flex justify-content-center align-items-center bg-primary"
-      style={{ height: "59.1333px" }}
+      className="vw-100 d-flex justify-content-center align-items-center"
+      style={{
+        height: "59.1333px",
+        border: "1px solid #ccc",
+        padding: "14px",
+      }}
     >
       <div className="vw-100 d-flex justify-content-between">
         <h5 className="col-md-2">Annual Conference</h5>
-        <div className="col-md-2">
+        <div className="col-md-2 d-flex align-items-center">
           <img
             src="/images/home-icon.png"
-            style={{ width: "16.6167px", height: "15px" }}
+            style={{
+              width: "16.6167px",
+              height: "15px",
+            }}
           />
-          yooooo
+          <ArrowDivider />
+          <span>Tickets</span>
+          <ArrowDivider />
+          <span>Details</span>
+          <ArrowDivider />
+          <span>Confirmation</span>
         </div>
         <div className="col-md-2">durp</div>
       </div>
