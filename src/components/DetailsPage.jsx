873966bea36856ddbe45f3f50334c8fdc4800c43
Brice <59327804+BriceWidger@users.noreply.github.com>
2024-12-08 14:49:24 -0500

    modified:   src/components/DetailsPage.jsx

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import CheckoutHeader from "./CheckoutHeader";
 
-const DetailsPage = () => {};
+const DetailsPage = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#f8f9fc",
+        height: "100vh",
+      }}
+    >
+      <CheckoutHeader />
+    </div>
+  );
+};
 
 export default DetailsPage;
