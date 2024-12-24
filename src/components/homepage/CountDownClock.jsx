e9f4c90246b44757197de278b3ae30c441d374c1
Brice <59327804+BriceWidger@users.noreply.github.com>
2024-12-24 01:09:56 -0500

    updated CountDownClock

--- src/components/homepage/CountDownClock.jsx
+++ src/components/homepage/CountDownClock.jsx
@@ -2,12 +2,7 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const CountDownClock = () => {
-  const [cd, setCD] = useState({
-    d: 2,
-    h: 20,
-    m: 1,
-    s: 26,
-  });
+  const [cd, setCD] = useState({ d: 2, h: 20, m: 1, s: 26 });
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -26,43 +21,57 @@ const CountDownClock = () => {
   return (
     <div
       className="content-block content-block-countdown content-block-theme39-countdown-1 active"
-      style={{ backgroundColor: "#e9e9e9", padding: "17px 0", height: 154 }}
+      style={{
+        backgroundColor: "#e9e9e9",
+        padding: "17px 0",
+        height: 154,
+        display: "flex",
+        alignItems: "center",
+      }}
     >
-      <div className="content-block-content d-flex justify-content-center align-items-center h-100">
-        <div className="container text-center">
-          <div className="container-inner">
-            <div className="ec-editor-block ec-editor-block-countdown">
+      <div className="container text-center">
+        <div
+          className="row d-flex justify-content-center"
+          style={{ flexWrap: "wrap" }}
+        >
+          {["days", "hours", "minutes", "seconds"].map((label, i) => (
+            <div
+              key={i}
+              className="col-md-2 col-xs-3 mb-2"
+              style={{ textAlign: "center" }}
+            >
               <div
-                className="row d-flex justify-content-center"
-                style={{ flexWrap: "wrap" }}
+                className="h1"
+                style={{
+                  fontSize: 60,
+                  margin: 0,
+                  fontWeight: "500",
+                  fontFamily: "Poppins, sans-serif",
+                }}
               >
-                <div className="col-md-2 col-md-offset-2 col-xs-3 mb-2">
-                  <div className="h1" style={{ fontSize: 60, margin: 0 }}>
-                    {fmtNum(cd.d)}
-                  </div>
-                  <span style={{ fontSize: 16 }}>days</span>
-                </div>
-                <div className="col-md-2 col-xs-3 mb-2">
-                  <div className="h1" style={{ fontSize: 60, margin: 0 }}>
-                    {fmtNum(cd.h)}
-                  </div>
-                  <span style={{ fontSize: 16 }}>hours</span>
-                </div>
-                <div className="col-md-2 col-xs-3 mb-2">
-                  <div className="h1" style={{ fontSize: 60, margin: 0 }}>
-                    {fmtNum(cd.m)}
-                  </div>
-                  <span style={{ fontSize: 16 }}>minutes</span>
-                </div>
-                <div className="col-md-2 col-xs-3 mb-2">
-                  <div className="h1" style={{ fontSize: 60, margin: 0 }}>
-                    {fmtNum(cd.s)}
-                  </div>
-                  <span style={{ fontSize: 16 }}>seconds</span>
-                </div>
+                {fmtNum(
+                  cd[
+                    label === "days"
+                      ? "d"
+                      : label === "hours"
+                      ? "h"
+                      : label === "minutes"
+                      ? "m"
+                      : "s"
+                  ]
+                )}
+                <span
+                  style={{
+                    fontSize: 16,
+                    fontFamily: "Poppins, sans-serif",
+                    display: "block",
+                  }}
+                >
+                  {label}
+                </span>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
