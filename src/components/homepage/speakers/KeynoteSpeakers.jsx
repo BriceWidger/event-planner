cb3083c70d47ac608dded16d4e859f6f7a0ab1a2
Brice <59327804+BriceWidger@users.noreply.github.com>
2024-12-24 04:21:53 -0500

    update KeynoteSpeakers

--- src/components/homepage/speakers/KeynoteSpeakers.jsx
+++ src/components/homepage/speakers/KeynoteSpeakers.jsx
@@ -24,7 +24,7 @@ const KeynoteSpeakers = () => (
                 className="keynote-title text-center"
                 style={{ fontSize: KEYNOTE_TITLE_FONT_SIZE }}
               >
-                Keynote Speakers
+                Keynote Speakers DUUUDE
               </h1>
               <p
                 className="keynote-subtitle text-center"
