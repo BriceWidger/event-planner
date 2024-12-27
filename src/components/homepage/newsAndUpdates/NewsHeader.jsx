73f5c776228133f4de6beb8c8f1b236aba2f781e
Brice <59327804+BriceWidger@users.noreply.github.com>
2024-12-26 19:13:29 -0500

    updated newsAndUpdates section

--- src/components/homepage/newsAndUpdates/NewsHeader.jsx
+++ src/components/homepage/newsAndUpdates/NewsHeader.jsx
@@ -5,14 +5,46 @@ const NewsHeader = () => {
   return (
     <div className="row text-center">
       <div className="col-md-8 offset-md-2">
-        <h2 className="mb-4">News & Updates</h2>
-        <p className="mb-5">
-          Cras posuere non dui eu tincidunt. Maecenas congue dignissim tortor,
-          eget euismod ex. Duis fringilla auctor faucibus. Donec pulvinar
-          venenatis nisi. Nam vitae lectus egestas, pretium ante sed, ultrices
-          ligula. Donec erat elit, semper eu risus et, gravida sollicitudin
-          enim. Nunc sit amet imperdiet tellus. Ut lacinia odio urna, non
-          eleifend odio porttitor in. Fusce vitae dictum dui, at cursus enim.
+        <h2
+          style={{
+            fontSize: "52px",
+            fontFamily: "'Unna', serif",
+            fontWeight: 400,
+            margin: 0,
+            lineHeight: "110%",
+          }}
+          className="mb-4"
+        >
+          News & Updates
+        </h2>
+        <p
+          style={{
+            color: "#888",
+            fontWeight: 400,
+            fontSize: "16px",
+            lineHeight: "150%",
+            fontFamily: "'Nunito Sans', sans-serif",
+            textAlign: "center",
+          }}
+          className="mb-5"
+        >
+          <span
+            style={{
+              color: "#888",
+              fontWeight: 400,
+              fontSize: "16px",
+              lineHeight: "150%",
+              fontFamily: "'Nunito Sans', sans-serif",
+              textAlign: "center",
+            }}
+          >
+            Cras posuere non dui eu tincidunt. Maecenas congue dignissim tortor,
+            eget euismod ex. Duis fringilla auctor faucibus. Donec pulvinar
+            venenatis nisi. Nam vitae lectus egestas, pretium ante sed, ultrices
+            ligula. Donec erat elit, semper eu risus et, gravida sollicitudin
+            enim. Nunc sit amet imperdiet tellus. Ut lacinia odio urna, non
+            eleifend odio porttitor in. Fusce vitae dictum dui, at cursus enim.
+          </span>
         </p>
       </div>
     </div>
--- src/components/homepage/newsAndUpdates/NewsItem.jsx
+++ src/components/homepage/newsAndUpdates/NewsItem.jsx
@@ -8,13 +8,40 @@ const NewsItem = ({ image, title, date, text }) => {
         <img className="img-fluid" src={image} />
       </div>
       <div className="col-md-6 mb-5">
-        <h5 className="mb-3">
-          <strong>{title}</strong>
+        <h5
+          style={{
+            fontWeight: "bold",
+            fontSize: "20px",
+            lineHeight: "110%",
+          }}
+          className="mb-3"
+        >
+          {title}
         </h5>
-        <h4 className="mb-3">
-          <strong>{date}</strong>
+        <h4
+          style={{
+            fontWeight: 700,
+            textTransform: "uppercase",
+            letterSpacing: "3px",
+            fontSize: "14px",
+            margin: 0,
+            lineHeight: "200%",
+          }}
+          className="mb-3"
+        >
+          {date}
         </h4>
-        <p>{text}</p>
+        <p
+          style={{
+            color: "#888",
+            fontWeight: 400,
+            fontSize: "16px",
+            lineHeight: "150%",
+            fontFamily: "'Nunito Sans', sans-serif",
+          }}
+        >
+          {text}
+        </p>
       </div>
     </div>
   );
