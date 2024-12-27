2deb08fad8cb40b26a88e9914c19e36159a04e50
Brice <59327804+BriceWidger@users.noreply.github.com>
2024-12-27 09:10:29 -0500

    fixed responsive design for NewsAndUpdatesContainer

--- src/components/homepage/newsAndUpdates/NewsAndUpdatesContainer.jsx
+++ src/components/homepage/newsAndUpdates/NewsAndUpdatesContainer.jsx
@@ -7,10 +7,21 @@ const NewsAndUpdatesContainer = () => {
   return (
     <div
       id="news-updates-container"
-      className="news-updates-container d-flex justify-content-center align-items-center"
-      style={{ height: 1075.97, backgroundColor: "#eaeaea" }}
+      className="news-updates-container d-flex flex-column justify-content-center align-items-center"
+      style={{
+        flex: 1,
+        backgroundColor: "#eaeaea",
+        overflow: "hidden",
+        padding: "2rem",
+      }}
     >
-      <div className="container mt-5 mb-5">
+      <div
+        className="container mt-5 mb-5"
+        style={{
+          maxHeight: "100%",
+          overflowY: "auto",
+        }}
+      >
         <NewsHeader />
         <NewsList />
       </div>
