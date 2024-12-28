c581928d01609fcd77ce24653525f687e73adc83
Brice <59327804+BriceWidger@users.noreply.github.com>
2024-12-28 07:57:57 -0500

    added global stylesheet (responsive.css) for media queries

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Use 'react-dom/client' for React 18+
 import App from "./App"; // Adjust the path if necessary
+import "./styles/responsive.css"; // Import the global stylesheet
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
 // Create a root element for React to render into
