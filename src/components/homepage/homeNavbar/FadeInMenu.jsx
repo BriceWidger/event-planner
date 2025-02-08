e3ebe5ed6cc5db321b4b5264f0b08cf846958f22
Brice Widger <bkw11@rocketmail.com>
2025-02-07 19:37:10 -0500

    updated fadein/fadeout for overlaymenu

--- src/components/homepage/homeNavbar/FadeInMenu.jsx
+++ src/components/homepage/homeNavbar/FadeInMenu.jsx
@@ -4,12 +4,15 @@ import OverlayMenu from "./OverlayMenu";
 
 const FadeInMenu = ({ showMenu, onHideMenu }) => {
   const [fadeIn, setFadeIn] = useState(false);
+  const [fadeOut, setFadeOut] = useState(false);
 
   useEffect(() => {
     if (showMenu) {
       setFadeIn(true);
+      setFadeOut(false);
     } else {
       setFadeIn(false);
+      setFadeOut(true);
     }
   }, [showMenu]);
 
@@ -22,12 +25,12 @@ const FadeInMenu = ({ showMenu, onHideMenu }) => {
         width: "100%",
         height: "100%",
         zIndex: 1000,
-        opacity: fadeIn ? 1 : 0,
-        transition: "opacity 0.5s ease-in-out",
+        opacity: fadeIn ? 1 : fadeOut ? 0 : 0,
+        transition: "opacity 0.4s ease-in-out",
         pointerEvents: fadeIn ? "auto" : "none",
       }}
     >
-      {fadeIn && <OverlayMenu onHideMenu={onHideMenu} />}
+      {(fadeIn || fadeOut) && <OverlayMenu onHideMenu={onHideMenu} />}
     </div>
   );
 };
